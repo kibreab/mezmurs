5146ff095c2f40a8967de0965fb50882d1b5e894
KB (Kibreab) <kibreabt@gmail.com>
2018-02-28 10:59:20 +0200

    Also playlist incase user is not logged in

--- app/assets/javascripts/components/playlists/_add_song_to_playlist_button.jsx
+++ app/assets/javascripts/components/playlists/_add_song_to_playlist_button.jsx
@@ -1,9 +1,12 @@
 var AddSongToPlaylistButton = React.createClass({
 
     render() {
+      
+      var targetId = this.props.current_user ? "#playlistModal" : "#logiModal"      
       return(    
+
         <div className="pull-right">
-            <i className={this.props.nowPlaying ? "fa fa-plus action-icon-active" : "fa fa-plus action-icon"} data-toggle="modal" data-target="#playlistModal"></i>
+            <i className={this.props.nowPlaying ? "fa fa-plus action-icon-active" : "fa fa-plus action-icon"} data-toggle="modal" data-target={targetId}></i>
         </div>           
       )
     }
--- app/assets/javascripts/components/songs/_song.js.jsx
+++ app/assets/javascripts/components/songs/_song.js.jsx
@@ -227,7 +227,7 @@ var Song = React.createClass({
                         <div>{likeButton} <span className={nowPlaying ? "likes-count-active" : "likes-count"}>{likesCount}</span></div>
                     </div>
                                                                
-                    <AddSongToPlaylistButton />
+                    <AddSongToPlaylistButton current_user={this.props.current_user} />
 
                     <div className="edit-song-area">
                         <div className="edit-button">
